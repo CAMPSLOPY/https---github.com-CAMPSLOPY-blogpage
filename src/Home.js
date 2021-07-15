3758de2c44803576417bc1d1c81bf5218c865cf9
CAMPSLOPY <54154401+CAMPSLOPY@users.noreply.github.com>
2021-07-15 16:17:36 +0100

    Created hooks

--- src/Home.js
+++ src/Home.js
@@ -1,31 +1,13 @@
-import { useEffect, useState } from "react";
+import {  useEffect, useState } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  const [isPending, setIsPending] = useState(true);
 
-  useEffect(() => {
-    setTimeout(() => {
-      fetch('http://localhost:3000/blogs')
-      .then(res => {
-        if(!res.ok){
-          throw Error('could not fetch the data for that resource')
-        }
-        return res.json();
-      })
-      .then(data => {
-        setIsPending(false);
-        setBlogs(data);
-      })
-      .catch(err => {
-        console.log(err.message);
-      })
-    }, 1000);
-  }, [])
-
+  const {data: blogs , isPending , error} = useFetch('http://localhost:3000/blogs');
   return (
     <div className="home">
+      {error && <div>{error}</div>}
       { isPending && <div>Loading...</div> }
       {blogs && <BlogList blogs={blogs} />}
     </div>

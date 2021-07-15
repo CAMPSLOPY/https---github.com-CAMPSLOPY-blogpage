f94931f9f7cebd7a7594d5dd65514b44cd77f2fe
CAMPSLOPY <54154401+CAMPSLOPY@users.noreply.github.com>
2021-07-15 16:24:33 +0100

    added more functionalities to the blog component

--- src/BlogList.js
+++ src/BlogList.js
@@ -2,7 +2,7 @@ const BlogList = ({ blogs }) => {
     return (
       <div className="blog-list">
         {blogs.map(blog => (
-          <div className="blog-preview" key={blog.id} >
+          <div className="blog-preview" key={blog.id}>
             <h2>{ blog.title }</h2>
             <p>Written by { blog.author }</p>
           </div>

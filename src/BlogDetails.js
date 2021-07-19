bbaa94ce81273f7ef0d1adfb090074169c143577
CAMPSLOPY <54154401+CAMPSLOPY@users.noreply.github.com>
2021-07-19 15:57:57 +0100

    blog details

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -4,10 +4,17 @@ import useFetch from "./useFetch";
 const  BlogDetails = () => {
 
     const { id } = useParams()
-    const {data , error, isPending} = useFetch('http://localhost:3001/blogs/' + id)
+    const {data: blog, error, isPending} = useFetch('http://localhost:3001/blogs/' + id)
     return (  
         <div className="blog-details">
            {isPending && <div>Loading...</div>}
+           {error && <div>{error}</div>}
+           {blog && (
+               <article>
+                   <h2>{blog.title}</h2>
+                   <p> Written by {blog.author}</p>
+               </article>
+           )}
         </div>
     );
 }

d07e10c7ccd520f5132e5171cb8aeda594713e24
CAMPSLOPY <54154401+CAMPSLOPY@users.noreply.github.com>
2021-07-19 15:41:39 +0100

    updated the fetch component

--- src/useFetch.js
+++ src/useFetch.js
@@ -6,8 +6,10 @@ const useFetch = (url) => {
     const [isPending, setIsPending] = useState(true);
     const [error, setError] = useState(null);
     useEffect(() => {
+
+      const abortCont = new AbortController();
         setTimeout(() => {
-          fetch(url)
+          fetch(url, {signal: abortCont.signal})
           .then(res => {
             if(!res.ok){
               throw Error('could not fetch the data for that resource')
@@ -20,14 +22,19 @@ const useFetch = (url) => {
             setError(null);
           })
           .catch(err => {
-            setIsPending(false);
-            setError(err.message);
+            if(err.name === 'AbortError'){
+              console.log('Fetch aborted')
+            }else {
+              setIsPending(false);
+              setError(err.message);
+            }
+            
           })
         }, 1000);
 
-        return () => AbortController.abort();
+        return () => abortCont.abort();
       }, [url]);
-      
+
       return {data , isPending , error}
 
 }

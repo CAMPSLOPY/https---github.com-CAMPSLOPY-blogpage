887af01974c4a78a12e361b9bc539561346ad9eb
CAMPSLOPY <54154401+CAMPSLOPY@users.noreply.github.com>
2021-07-19 15:03:55 +0100

    added Hook Link to the  blog preview

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,10 +1,15 @@
+import { Link } from "react-router-dom";
+
 const BlogList = ({ blogs }) => {
     return (
       <div className="blog-list">
         {blogs.map(blog => (
           <div className="blog-preview" key={blog.id}>
+            <Link>
             <h2>{ blog.title }</h2>
             <p>Written by { blog.author }</p>
+            </Link>
+        
           </div>
         ))}
       </div>
